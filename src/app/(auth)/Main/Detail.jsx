cbe3a28b21dc936a91743eb72af522e8e8b39bcd
gelim <b4winter@qraod.com>
2023-09-15 17:56:42 +0900

    브랜치 테스트 (0915)

---
+++ src/app/(auth)/Main/Detail.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Detail(){
+
+    return(
+        <div>디테일페이지 작업</div>
+    )
+}
---
+++ src/app/(auth)/Main/Main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Main(){
+
+    return(
+        <div>메인페이지 작업</div>
+    )
+}
